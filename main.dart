2f8a5628063c5748db797e426750b7cd218572c1
shidos <mohammedtamim249@gmail.com>
2024-08-27 03:29:50 +0300

    change scaffold and container

--- main.dart
+++ main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home : Scaffold(
+        Container(
+          
+        )
+      )
     );
   }
 }
